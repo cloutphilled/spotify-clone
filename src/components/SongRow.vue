e9d60061dbaead7a1fae9ab592210170baddf5eb
Phillip Friis Petersen <phillipfriispetersen@gmail.com>
2023-04-05 10:27:06 +0200

    quick commit

---
+++ src/components/SongRow.vue
@@ -0,0 +1,23 @@
+<script setup>
+    import {ref, toRefs, onMounted} from 'vue'
+    import Play from 'vue-material-design-icons/Play.vue'
+    import Pause from 'vue-material-design-icons/Pause.vue'
+    import Heart from 'vue-material-design-icons/Heart.vue'
+
+    let isHover = ref(false)
+    let isTrackTime = ref(null)
+
+    const props = defineProps({
+        track: Object,
+        artist: Object,
+        index: Number,
+    })
+
+    const { track, artist, index } = toRefs(props)
+</script>
+
+<template>
+    <div>
+
+    </div>
+</template>
---
+++ src/views/SearchView.vue
@@ -0,0 +1,20 @@
+<script setup>
+    import CategorySelect from '../components/CategorySelect.vue'
+</script>
+
+<template>
+    <div class="p-8">
+        <button
+            type="button"
+            class="text-white text-2xl font-semibold hover:underline cursor-pointer"
+        >
+            Podcasts to make you smarter
+        </button>
+
+        <div class="py-1.5"></div>
+        <div class="grid xl:grid-cols-5 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 gap-6">
+            <CategorySelect category="Podcasts" image="https://picsum.photos/id/40/300/300" />
+        </div>
+        
+    </div>
+</template>
